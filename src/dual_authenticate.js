abe31a5f3ff4d41d656a5dfa1b6e8b3d75a18846
Ruzli Rifdhi <ruzlirifdhi@Ruzlis-MacBook-Pro.local>
2019-11-17 12:44:52 -0500

    dual authenticate using Speakeasy

---
+++ src/dual_authenticate.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// Using Speakeasy for dual authentication
+// General idea; generate secret key and QR Code with either Google Authenticate or Duo
+function Authenticate(){
+  var speakeasy = require('speakeasy');
+  var key = speakeasy.generateSeceret();
+}
